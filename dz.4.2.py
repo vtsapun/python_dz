80cb19e1a447f12516fca125a4ff5f6727be2501
vtsapun <vtsapun@gmail.com>
2024-06-03 18:52:12 +0300

    ss

---
+++ dz.4.2.py
@@ -0,0 +1,20 @@
+lst = [3,3,3,3,3,3,3]
+i = 0
+new_lst = []
+if lst:
+    i = 0
+    while i < len(lst):
+        l = lst[i]
+        i += 2
+        new_lst.append(l)
+        x = (lst[-1])
+        continue
+    print(sum(new_lst) * x)
+else:
+    print(new_lst)
+
+
+
+
+
+
