d6e3574ce7f802408482896ad936d2c6851e9bb4
vtsapun <vtsapun@gmail.com>
2024-06-06 15:11:19 +0300

    ss

---
+++ dz5.2.py
@@ -0,0 +1,18 @@
+while True:
+    first_number = int(input('Введите первое число: '))
+    second_number = int(input('Введите второе число: '))
+    third_do = input('Введите действие')
+    if third_do == '+':
+        print(first_number + second_number)
+    elif third_do == '-':
+        print(first_number - second_number)
+    elif third_do == '/' and second_number != 0:
+        print(first_number / second_number)
+    elif third_do == '*':
+        print(first_number * second_number)
+    else:
+        print('На ноль делить нельзя')
+    x = input('Продолжить работу?').lower()
+    if x != 'y':
+        print('Завершение работы калькулятора ')
+        break
