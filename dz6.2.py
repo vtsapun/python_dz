5f5880c5c924c1b064e2b6c379f22dae7d09f256
vtsapun <vtsapun@gmail.com>
2024-06-12 15:54:41 +0300

    ss

--- dz6.2.py
+++ dz6.2.py
@@ -3,10 +3,11 @@ seconds_input = int(input("Введіть кількість секунд: "))
 if seconds_input < 0 or seconds_input >= 8640000:
     print("Невірне значення. Введіть число більше або дорівнює 0 і менше ніж 8640000.")
 else:
-    days, remainder = divmod(seconds_input, 24 * 60 * 60)
-    hours, remainder = divmod(remainder, 60 * 60)
-    minutes, seconds = divmod(remainder, 60)
+    days = seconds_input // (24 * 60 * 60)
+    hours = (seconds_input % (24 * 60 * 60)) // (60 * 60)
+    minutes = (seconds_input % (60 * 60)) // 60
+    seconds = seconds_input % 60
 
-    time_str = f"{int(days)} {'день' if days == 1 else 'дні' if 2 <= days <= 4 else 'днів'}:{hours:02d}:{minutes:02d}:{seconds:02d}"
+    x = f"{int(days)} {'день' if days == 1 else 'дні' if 2 <= days <= 4 else 'днів'}:{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
 
-    print(time_str)
+    print(x)
