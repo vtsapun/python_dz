49e8c753668ec853f576d939f220620edf8e01ba
vtsapun <vtsapun@gmail.com>
2024-06-12 16:16:37 +0300

    ss

---
+++ dz6.3.py
@@ -0,0 +1,9 @@
+x = int(input("Введіть ціле число: "))
+while x > 9:
+    number = [int(digit) for digit in str(x)]
+    product = 1
+    for digit in number:
+        product *= digit
+    x = product
+    print(x)
+print(x)
